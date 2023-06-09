b0a3295edb66ace79d9f7d41c4b0c0f3f689de07
Gulevskii Ivan <ithirteengw@gmail.com>
2023-06-10 00:27:42 +0700

    Add koin to application class

--- app/src/main/java/com/ithirteeng/dishesshopproject/App.kt
+++ app/src/main/java/com/ithirteeng/dishesshopproject/App.kt
@@ -1,9 +1,20 @@
 package com.ithirteeng.dishesshopproject
 
 import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.logger.Level
+import org.koin.dsl.koinApplication
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        koinApplication {
+            androidLogger(Level.ERROR)
+            androidContext(this@App)
+
+            modules(
+            )
+        }
     }
 }
