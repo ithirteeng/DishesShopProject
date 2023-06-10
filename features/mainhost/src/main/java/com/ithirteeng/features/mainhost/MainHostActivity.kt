6119e767cf472e26649b8ce1069fbfd729a1b8c5
Gulevskii Ivan <ithirteengw@gmail.com>
2023-06-10 16:11:06 +0700

    Add UI of mainhost

---
+++ features/mainhost/src/main/java/com/ithirteeng/features/mainhost/MainHostActivity.kt
@@ -0,0 +1,11 @@
+package com.ithirteeng.features.mainhost
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainHostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_host)
+    }
+}
