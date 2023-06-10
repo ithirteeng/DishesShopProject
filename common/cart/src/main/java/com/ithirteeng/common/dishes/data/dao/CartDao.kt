2b9509196c31f7a29790e151a2509d94be64ad04
Gulevskii Ivan <ithirteengw@gmail.com>
2023-06-10 23:28:18 +0700

    Start implementing logic of saving/deleting/updating cart

---
+++ common/cart/src/main/java/com/ithirteeng/common/dishes/data/dao/CartDao.kt
@@ -0,0 +1,28 @@
+package com.ithirteeng.common.dishes.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.ithirteeng.common.dishes.data.entity.CartEntity
+import retrofit2.http.DELETE
+
+@Dao
+interface CartDao {
+    @Insert
+    fun addDishToCart(cartEntity: CartEntity)
+
+    @Query(
+        "UPDATE cart_table " +
+                "SET quantity = :newQuantity " +
+                "WHERE dishId = :dishId"
+    )
+    fun changeQuantity(dishId: String, newQuantity: Int)
+
+    @DELETE
+    fun deleteDishFromCart(cartEntity: CartEntity)
+
+    @Query("SELECT * FROM cart_table")
+    fun getAllDishesFromCart(): List<CartEntity>?
+
+
+}
---
+++ common/cart/src/main/java/com/ithirteeng/common/dishes/data/entity/CartEntity.kt
@@ -0,0 +1,16 @@
+package com.ithirteeng.common.dishes.data.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "cart_table")
+data class CartEntity(
+    @PrimaryKey
+    val dishId: String,
+    val name: String,
+    val price: Int,
+    val weight: Int,
+    val description: String,
+    val imageUrl: String,
+    val quantity: Int = 1,
+)
